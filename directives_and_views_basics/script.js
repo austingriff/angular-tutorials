e32ead76151b0015872e0f6af651f10c61c9dc0a
Austingriff <austingriff@live.com>
2017-03-21 17:12:05 -0500

    added tutorial for using directives to create views

---
+++ directives_and_views_basics/script.js
@@ -0,0 +1,35 @@
+// Code goes here
+
+(function() {
+  
+  var app = angular.module("githubViewer", []);
+  
+  var MainController = function($scope, $http) {
+
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+      $http.get($scope.user.repos_url)
+           .then(onRepos, onError);
+    };
+    
+    var onRepos = function(response){
+      $scope.repos = response.data;
+    };
+
+    var onError = function(response) {
+      $scope.error = "Could not fetch data.";
+    };
+      
+    $scope.search = function(username){
+      $http.get("https://api.github.com/users/" + username)
+           .then(onUserComplete, onError);
+    }
+
+    $scope.message = "GitHub Viewer";
+    $scope.repoSortOrder = "-stargazers_count";
+
+  };
+  
+  app.controller("MainController", ["$scope", "$http", MainController]);
+  
+}());
